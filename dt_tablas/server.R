c84e90661c45ac0703c5eb60b0f07db9bf82da6c
carlos <chzelada@gmail.com>
2020-09-10 01:34:09 +0000

    codigo que muestra donde el usuario hace click enun tabla

--- dt_tablas/server.R
+++ dt_tablas/server.R
@@ -19,7 +19,75 @@ output$tabla_1 <- DT::renderDataTable({
 })
 
 output$tabla_2 <- DT::renderDataTable({
-    diamonds %>% DT::datatable(filter = 'top')
+    diamonds %>% 
+        mutate(vol= x*y*z,
+               vol_promedio = mean(vol),
+               volp=vol/vol_promedio-1) %>% 
+        DT::datatable(filter = 'top') %>% 
+        formatCurrency(columns = 'price',
+                       currency = '$') %>% 
+        formatPercentage('volp',digits = 2)
 })
+
+
+output$tabla_3 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = 'single')
+})
+
+
+output$output_1 <- renderPrint({
+    input$tabla_3_rows_selected
+})
+
+output$tabla_4 <- DT::renderDataTable({
+    mtcars %>% DT::datatable()
+})
+
+
+output$output_2 <- renderPrint({
+    input$tabla_4_rows_selected
+})
+
+output$tabla_5 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = list(mode='single',
+                                              target='column'))
+})
+
+
+output$output_3 <- renderPrint({
+    input$tabla_5_columns_selected
+})
+
+output$tabla_6 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = list(mode='multiple',
+                                              target='column'))
+})
+
+
+output$output_4 <- renderPrint({
+    input$tabla_6_columns_selected
+})
+
+
+output$tabla_7 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = list(mode='single',
+                                              target='cell'))
+})
+
+
+output$output_5 <- renderPrint({
+    input$tabla_7_cells_selected
+})
+
+output$tabla_8 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = list(mode='multiple',
+                                              target='cell'))
+})
+
+
+output$output_6 <- renderPrint({
+    input$tabla_8_cells_selected
+})
+
 
 })
--- dt_tablas/ui.R
+++ dt_tablas/ui.R
@@ -16,7 +16,38 @@ shinyUI(fluidPage(
                  h1("Agregar filtros"),
                  fluidRow( column(12, DT::dataTableOutput("tabla_2")))
                  ),
-        tabPanel("click en tablas",'2')
+        tabPanel("click en tablas",
+                 fluidRow(column(6,
+                                 h2('Single Select row'),
+                                 DT::dataTableOutput('tabla_3'),
+                                 verbatimTextOutput('output_1')),
+                          column(6,
+                                 h2('Multiple Select rows'),
+                                 DT::dataTableOutput('tabla_4'),
+                                 verbatimTextOutput('output_2')
+                                 )
+                          ),
+                 fluidRow(column(6,
+                                 h2('Single Select cols'),
+                                 DT::dataTableOutput('tabla_5'),
+                                 verbatimTextOutput('output_3')
+                                 ),
+                          column(6,
+                                 h2('Multiple Select cols'),
+                                 DT::dataTableOutput('tabla_6'),
+                                 verbatimTextOutput('output_4')
+                                 )
+                          ),
+                 fluidRow(column(6,
+                                 DT::dataTableOutput('tabla_7'),
+                                 verbatimTextOutput('output_5')
+                                 ),
+                          column(6,
+                                 DT::dataTableOutput('tabla_8'),
+                                 verbatimTextOutput('output_6')
+                                 )
+                          )
+                 )
     )
 
 ))
